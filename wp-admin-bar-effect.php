2ee2885706ca9e49c8542c5e42ed9f6ac1936403
kallookoo <747817+kallookoo@users.noreply.github.com>
2020-11-20 11:44:57 +0100

    Version 2.3.1

--- wp-admin-bar-effect.php
+++ wp-admin-bar-effect.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * @package WP Admin Bar Effect (wabe)
- * @version 2.3
+ * @version 2.3.1
  */
 /*
 Plugin Name: WP Admin Bar Effect (wabe)
 Plugin URI: http://wordpress.org/extend/plugins/wp-admin-bar-effect/
 Description: Add effect slideDown to desktop top bar 
 Author: Sergio P.A. ( 23r9i0 )
-Version: 2.3
+Version: 2.3.1
 Author URI: http://dsergio.com/
 Copyright 2011-2013  Sergio Prieto Alvarez  (email : info@dsergio.com)
 */
@@ -16,7 +16,7 @@ Copyright 2011-2013  Sergio Prieto Alvarez  (email : info@dsergio.com)
 if( !class_exists( 'WP_ADMIN_BAR_EFFECT' ) ) : 
 class WP_ADMIN_BAR_EFFECT {
 	private 
-		$wabe_version = '2.3',
+		$wabe_version = '2.3.1',
 		$wabe_options,
 		$wabe_options_defaults = array(
 			'wabe_active_link'		=> '2',
@@ -43,8 +43,8 @@ class WP_ADMIN_BAR_EFFECT {
 		if( isset( $this->wabe_options['icolink'] ) ){
 			foreach( $this->wabe_options as $option => $value ){
 				foreach( $this->wabe_options_defaults as $doption => $dvalue ){
-					if( $option == $doption ){
-						$this->wabe_options_defaults[$option] = $this->wabe_options[$option];
+					if( $option == substr($doption, 5 ) ){
+						$this->wabe_options_defaults[$doption] = $this->wabe_options[$option];
 					} else {
 						if( !isset( $this->wabe_options['actlink'] ) )
 							$this->wabe_options_defaults['wabe_active_link'] = '1';		
